3bea1fbef1b1dd49910386dbaa522c720d324f80
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2025-02-01 19:24:41 +0700

    Fix hook ID

--- app/Provider.php
+++ app/Provider.php
@@ -131,7 +131,7 @@ class Provider extends ServiceProvider implements Hookable
 				[$registry, 'register'],
 				10,
 				1,
-				['id' => 'settings-provider/rest_api_init' . $group],
+				['id' => 'settings-provider/rest_api_init_' . $group],
 			);
 		}
 	}
